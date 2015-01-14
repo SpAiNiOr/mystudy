6342c02e3da121e1b730720f7bfd61c022141b85
spainior <spainior@gmail.com>
2015-01-14 23:58:21 +0800

    Learned test method

--- learning/test/my_math.py
+++ learning/test/my_math.py
@@ -6,3 +6,10 @@ def square(x):
     4
     '''
     return x*x
+
+def product(x,y):
+    #if x==7 and y ==9:
+    #    return 'An insidious bug has surfaced'
+    #else:
+    #    return x*y
+    return x*y
---
+++ learning/test/myprofile.py
@@ -0,0 +1,3 @@
+import profile
+from my_math import product
+profile.run('product(1,2)','123.txt')
