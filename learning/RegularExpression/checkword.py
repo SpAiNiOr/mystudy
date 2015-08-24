710c6add6049d4cda6572b537d53cc3521183049
Junbo Wang <juniwang@microsoft.com>
2015-08-24 17:23:46 +0800

    Update checkword.py

--- learning/RegularExpression/checkword.py
+++ learning/RegularExpression/checkword.py
@@ -1,9 +1,26 @@
 __author__ = 'Ice'
 import re
 
-word = raw_input('Pig Latin:')
 p = re.compile(r'.*\d+')
-m = p.search(word)
+
+def read_input()
+    f=True
+    while True:
+        m = 'Pig Latin:' if f else "your input is not word,please try again:"
+        word = raw_input(m)
+        f= False
+        m = p.search(word)
+        if m is None:
+            return word
+
+
+word = None
+while True:
+    m = 'Pig Latin:'
+    word = raw_input(m)
+    m = p.search(word)
+    if m is None:
+        break
 
 
 def translator(string):
