4e7bc054559b87debe3dec3cd8f07a44afea33d6
spainior <ice_shi@163.com>
2015-10-01 23:08:08 +0800

    add two player mode

--- learning/test/battleship_v2.py
+++ learning/test/battleship_v2.py
@@ -19,6 +19,7 @@ def print_battlefiled(battlefiled):
     for row in battlefiled:
         print " ".join(row)
 
+
 print print_battlefiled(board)
 
 # create battle ships
@@ -32,7 +33,7 @@ def create_ship(size, number=1):
             ship_row = randint(0, (size - 1))
             ship_col = randint(0, (size - 1))
             if [ship_row, ship_col] not in battle_ship:
-                battle_ship.append([ship_row,ship_col])
+                battle_ship.append([ship_row, ship_col])
                 break
     return
 
@@ -42,15 +43,14 @@ create_ship(size, ship_number)
 # main program
 print "Game Start"
 
-for n in range(ship_number * 4):
-    guess_row = int(raw_input("Please choose row number:"))
-    guess_col = int(raw_input("Please choose col number:"))
+
+# based on player's input, compare with ship's coordinate which stored in battle_ship
+def player_guess(guess_row, guess_col):
     if [guess_row, guess_col] in battle_ship and board[guess_row][guess_col] == "O":
         board[guess_row][guess_col] = "K"
         print print_battlefiled(board)
         ship_number -= 1
-        if ship_number == 0:
-            break
+        return True
     else:
         if (guess_row < 0 or guess_row >= size) or (guess_col < 0 or guess_col >= size):
             print "Your choice is out range, please choose the number under :", size - 1
@@ -59,6 +59,32 @@ for n in range(ship_number * 4):
         else:
             board[guess_row][guess_col] = "X"
             print "You have missed the ship!"
+        return False
+
+# ask user to input their name.
+player_a = raw_input("please input first player's name: ") or "player_1"
+player_b = raw_input("please input second player's name: ") or "player_2"
+player_a_score = 0
+player_b_score = 0
+for n in range(ship_number * 8):
+    if n % 2 == 0:
+        print "It's %s's turn" % player_a
+    else:
+        print "It's %s's turn" % player_b
+    print
+    guess_row = int(raw_input("Please choose row number:"))
+    guess_col = int(raw_input("Please choose col number:"))
+    result = player_guess(guess_row, guess_row)
     print print_battlefiled(board)
-    if n == ship_number * 4 - 1:
+    if n % 2 is False and result:
+        player_a_score += 1
+    if n % 2 and result:
+        player_a_score += 1
+    if n < ship_number * 8 - 1:
+        print "%s's  score is " % player_a, player_a_score
+        print "%s's  score is " % player_b, player_b_score
+    if n == ship_number * 8 - 1 or ship_number == 0:  # if iterate over or ship all sank
         print "Game Over"
+        print "%s's final score is " % player_a, player_a_score
+        print "%s's final score is " % player_b, player_b_score
+        break
