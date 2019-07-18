46880eb7fb990800d5883ea2d0efdef62308ffc2
spainior <ice_shi@163.com>
2019-07-18 14:37:25 +0800

    twosum

---
+++ learning/java/Temp/Temp.java
@@ -0,0 +1,15 @@
+// this is a temp java file
+import java.util.*;
+
+public class Temp  {
+
+    public Temp() {
+        System.out.println("This is it");
+
+    }
+
+    public static void main(String[] args) {
+        Temp er = new Temp();
+    }
+
+}
