be9fd7d5f25f5b8ef749c4db25eb3387e9ff64ec
spainior <ice_shi@163.com>
2020-07-22 22:40:18 +0800

    Annotation

---
+++ learning/csharp/AspDotNet/Razor/Annotation.cs
@@ -0,0 +1,35 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorCountry.Models
+{
+    public class Country
+    {
+        [Required]
+        [Display(Name = "Code")]
+        [StringLength(2, MinimumLength = 2)]
+        [RegularExpression(@"[A-Z]+", ErrorMessage = "Only upper case characters are allowed.")]
+        public string ID { get; set; }
+
+        [Required]
+        [Display(Name = "Continent")]
+        [StringLength(2, MinimumLength = 2)]
+        [RegularExpression(@"[A-Z]", ErrorMessage = "Only upper case charactor allowed.")]
+        public string ContinentID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Range(1, 10000000000)]
+        [DisplayFormat(DataFormatString = "{0:N0}", ApplyFormatInEditMode = true)]
+        public int? Population { get; set; }
+
+        public int? Area { get; set; }
+
+        [Display(Name = "UN Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Range(typeof(DateTime), "10/24/1945", "1/1/2100", ErrorMessage = "The United Nations was created 10/24/1945.")]
+        [DataType(DataType.Date)]
+        public DateTime? UnitedNationsDate { get; set; }
+    }
+}
